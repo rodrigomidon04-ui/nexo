e4e7d5a84f314d1a1159712e439655098bba616b
rodrigomidon04-ui <rodrigomidon04@gmail.com>
2026-08-19 05:50:28 -0300

    NEXO: bust UI asset cache after navigation/layout fixes

--- build.js
+++ build.js
@@ -10,9 +10,9 @@ const scripts = [
   ['nexo32.js?v=3.2', '<script src="./nexo32.js?v=3.2"></script>'],
   ['nexo33.js?v=3.3', '<script src="./nexo33.js?v=3.3"></script>'],
   ['nexo33134.js?v=3.4', '<script src="./nexo33134.js?v=3.4"></script>'],
-  ['nexo-ui-controller.js?v=1.1.0', '<script src="./nexo-ui-controller.js?v=1.1.0"></script>'],
-  ['nexo-ui-runtime-fix.js?v=1.0.0', '<script src="./nexo-ui-runtime-fix.js?v=1.0.0"></script>'],
-  ['nexo-ui-final-adjustments.js?v=1.0.0', '<script src="./nexo-ui-final-adjustments.js?v=1.0.0"></script>']
+  ['nexo-ui-controller.js?v=1.1.1', '<script src="./nexo-ui-controller.js?v=1.1.1"></script>'],
+  ['nexo-ui-runtime-fix.js?v=1.0.1', '<script src="./nexo-ui-runtime-fix.js?v=1.0.1"></script>'],
+  ['nexo-ui-final-adjustments.js?v=1.0.1', '<script src="./nexo-ui-final-adjustments.js?v=1.0.1"></script>']
 ];
 
 for (const [marker, tag] of scripts) {
